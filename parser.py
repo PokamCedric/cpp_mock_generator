3ccebc4569da1e14a958facc5fb71dee0d153d6c
Cedric Joel Ngameni Pokam <cngamenipokam@gmail.com>
2024-07-05 23:35:42 +0200

    refactore parser

--- parser.py
+++ parser.py
@@ -1,136 +1,135 @@
 import yaml
 from clang.cindex import Config, Index, CursorKind, TokenKind
 
-# Replace '/usr/lib/llvm-13/lib' with the actual path where libclang-13.so is located
+# Set the library path for libclang
 Config.set_library_path('/usr/lib/llvm-13/lib')
 
-def parse_header(file_paths):
-    # Initialize the Clang index
-    index = Index.create()
+def initialize_index():
+    """Initialize the Clang index."""
+    return Index.create()
 
-    output_data = []
-    processed_classes = {}  # Dictionary to store processed classes by name
+def process_function(node):
+    """Process a function declaration."""
+    return {
+        'type': 'Function',
+        'name': node.spelling,
+        'return_type': node.result_type.spelling,
+        'parameters': [
+            {'name': arg.spelling, 'type': arg.type.spelling}
+            for arg in node.get_arguments()
+        ]
+    }
 
-    def process_translation_unit(tu):
-        # Function to recursively visit nodes in the AST
-        def visit_node(node, depth=0):
-            node_data = {}
-            if node.kind == CursorKind.FUNCTION_DECL and not node.semantic_parent.kind in (CursorKind.CLASS_DECL, CursorKind.STRUCT_DECL):
-                # Collect function details
-                node_data = {
-                    'type': 'Function',
-                    'name': node.spelling,
-                    'return_type': node.result_type.spelling,
-                    'parameters': [
-                        {'name': arg.spelling, 'type': arg.type.spelling}
-                        for arg in node.get_arguments()
-                    ]
-                }
+def process_class(node, processed_classes):
+    """Process a class declaration."""
+    class_data = {
+        'type': 'Class',
+        'name': node.spelling,
+        'base_classes': [],
+        'members': [],
+        'methods': [],
+        'static_members': []
+    }
 
-            elif node.kind == CursorKind.CLASS_DECL:
-                # Collect class details
-                node_data = {
-                    'type': 'Class',
-                    'name': node.spelling,
-                    'base_classes': [],
-                    'members': [],
-                    'methods': [],
-                    'static_members': []
-                }
+    for base in node.get_children():
+        if base.kind == CursorKind.CXX_BASE_SPECIFIER:
+            class_data['base_classes'].append(base.type.spelling)
 
-                # Check for base classes (inheritance)
-                for base in node.get_children():
-                    if base.kind == CursorKind.CXX_BASE_SPECIFIER:
-                        node_data['base_classes'].append(base.type.spelling)
+    for child in node.get_children():
+        if child.kind == CursorKind.CXX_METHOD:
+            method_data = process_method(child)
+            class_data['methods'].append(method_data)
+        elif child.kind == CursorKind.FIELD_DECL:
+            member_data = process_member(child)
+            if member_data['is_static']:
+                class_data['static_members'].append(member_data)
+            else:
+                class_data['members'].append(member_data)
 
-                # Visit children to find methods and members
-                for child in node.get_children():
-                    if child.kind == CursorKind.CXX_METHOD:
-                        method_data = {
-                            'type': 'Method',
-                            'name': child.spelling,
-                            'return_type': child.result_type.spelling,
-                            'parameters': [
-                                {'name': arg.spelling, 'type': arg.type.spelling}
-                                for arg in child.get_arguments()
-                            ],
-                            'is_virtual': 'virtual' in child.type.spelling,
-                            'is_static': child.is_static_method(),
-                            'is_const': child.is_const_method()
-                        }
-                        node_data['methods'].append(method_data)
-                    elif child.kind == CursorKind.FIELD_DECL:
-                        member_data = {
-                            'type': 'Member',
-                            'name': child.spelling,
-                            'type': child.type.spelling,
-                            'is_static': child.is_static_field()
-                        }
-                        if member_data['is_static']:
-                            node_data['static_members'].append(member_data)
-                        else:
-                            node_data['members'].append(member_data)
+    processed_classes[node.spelling] = class_data
+    return class_data
 
-                processed_classes[node.spelling] = node_data
+def process_method(node):
+    """Process a method declaration."""
+    return {
+        'type': 'Method',
+        'name': node.spelling,
+        'return_type': node.result_type.spelling,
+        'parameters': [
+            {'name': arg.spelling, 'type': arg.type.spelling}
+            for arg in node.get_arguments()
+        ],
+        'is_virtual': 'virtual' in node.type.spelling,
+        'is_static': node.is_static_method(),
+        'is_const': node.is_const_method()
+    }
 
-            elif node.kind == CursorKind.NAMESPACE:
-                # Collect namespace details
-                namespace_data = {
-                    'type': 'Namespace',
-                    'name': node.spelling,
-                    'children': []
-                }
-                for child in node.get_children():
-                    namespace_data['children'].append(visit_node(child, depth + 1))
-                return namespace_data
+def process_member(node):
+    """Process a member declaration."""
+    return {
+        'type': 'Member',
+        'name': node.spelling,
+        'type': node.type.spelling,
+        'is_static': node.is_static_field()
+    }
 
-            elif node.kind == CursorKind.ENUM_DECL:
-                # Collect enum details
-                enum_data = {
-                    'type': 'Enum',
-                    'name': node.spelling,
-                    'values': [
-                        {'name': enum_value.spelling}
-                        for enum_value in node.get_children()
-                        if enum_value.kind == CursorKind.ENUM_CONSTANT_DECL
-                    ]
-                }
-                output_data.append(enum_data)
+def process_namespace(node):
+    """Process a namespace declaration."""
+    namespace_data = {
+        'type': 'Namespace',
+        'name': node.spelling,
+        'children': []
+    }
+    for child in node.get_children():
+        namespace_data['children'].append(process_node(child))
+    return namespace_data
 
-            elif node.kind == CursorKind.TYPEDEF_DECL:
-                # Collect typedef details
-                typedef_data = {
-                    'type': 'Typedef',
-                    'name': node.spelling,
-                    'underlying_type': node.underlying_typedef_type.spelling
-                }
-                output_data.append(typedef_data)
+def process_enum(node):
+    """Process an enum declaration."""
+    return {
+        'type': 'Enum',
+        'name': node.spelling,
+        'values': [
+            {'name': enum_value.spelling}
+            for enum_value in node.get_children()
+            if enum_value.kind == CursorKind.ENUM_CONSTANT_DECL
+        ]
+    }
 
-            elif node.kind == CursorKind.MACRO_DEFINITION:
-                # Collect macro details
-                macro_data = {
-                    'type': 'Macro',
-                    'name': node.spelling,
-                    'value': ''.join([t.spelling for t in node.get_tokens() if t.kind == TokenKind.LITERAL])
-                }
-                output_data.append(macro_data)
+def process_typedef(node):
+    """Process a typedef declaration."""
+    return {
+        'type': 'Typedef',
+        'name': node.spelling,
+        'underlying_type': node.underlying_typedef_type.spelling
+    }
 
-            if node_data:
-                output_data.append(node_data)
+def process_macro(node):
+    """Process a macro definition."""
+    return {
+        'type': 'Macro',
+        'name': node.spelling,
+        'value': ''.join([t.spelling for t in node.get_tokens() if t.kind == TokenKind.LITERAL])
+    }
 
-            # Visit children
-            for child in node.get_children():
-                visit_node(child, depth + 1)
+def process_node(node):
+    """Process a generic AST node."""
+    if node.kind == CursorKind.FUNCTION_DECL and not node.semantic_parent.kind in (CursorKind.CLASS_DECL, CursorKind.STRUCT_DECL):
+        return process_function(node)
+    elif node.kind == CursorKind.CLASS_DECL:
+        return process_class(node, processed_classes)
+    elif node.kind == CursorKind.NAMESPACE:
+        return process_namespace(node)
+    elif node.kind == CursorKind.ENUM_DECL:
+        return process_enum(node)
+    elif node.kind == CursorKind.TYPEDEF_DECL:
+        return process_typedef(node)
+    elif node.kind == CursorKind.MACRO_DEFINITION:
+        return process_macro(node)
+    return None
 
-        # Start visiting nodes from the root of the AST
-        visit_node(tu.cursor)
-
-    # Process each file
-    for file_path in file_paths:
-        translation_unit = index.parse(file_path, args=['-x', 'c++', '-std=c++14'])
-        process_translation_unit(translation_unit)
-
-    # Consolidate class inheritance
+def consolidate_classes(output_data, processed_classes):
+    """Consolidate class inheritance in the output data."""
     for class_data in output_data:
         if class_data['type'] == 'Class':
             for base_class in class_data['base_classes']:
@@ -140,9 +139,29 @@ def parse_header(file_paths):
                     class_data['methods'] = base_class_data['methods'] + class_data['methods']
                     class_data['static_members'] = base_class_data['static_members'] + class_data['static_members']
 
+def parse_header(file_paths):
+    """Parse the header files and extract relevant information."""
+    index = initialize_index()
+
+    output_data = []
+    global processed_classes
+    processed_classes = {}
+
+    def process_translation_unit(tu):
+        for node in tu.cursor.get_children():
+            node_data = process_node(node)
+            if node_data:
+                output_data.append(node_data)
+
+    for file_path in file_paths:
+        tu = index.parse(file_path, args=['-x', 'c++', '-std=c++14'])
+        process_translation_unit(tu)
+
+    consolidate_classes(output_data, processed_classes)
+
     return output_data
 
 def save_to_yaml(data, output_file):
-    # Write the output data to a YAML file
+    """Save the parsed data to a YAML file."""
     with open(output_file, 'w') as file:
         yaml.dump(data, file, sort_keys=False)
